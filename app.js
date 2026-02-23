bac865985726c1b6328eebed4621c052c255252b
Soportetriskelion <soporte@3triskelion.com>
2026-02-23 16:44:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const verifyToken = process.env.tokenbot;
 
 // Route for GET requests
 app.get('/', (req, res) => {
-  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token } = req.query;
+  const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.tokenbot': token } = req.query;
 
   if (mode === 'subscribe' && token === verifyToken) {
     console.log('WEBHOOK VERIFIED');
